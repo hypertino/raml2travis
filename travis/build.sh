9bdf9c51f80809769717fa85d7c1f2d42186e652
Maga Abdurakhmanov <maga@inn.eu>
2016-02-05 15:32:05 +0300

    * working on travis -> sonatype deploy

--- travis/build.sh
+++ travis/build.sh
@@ -4,11 +4,16 @@ set -e
 cd raml-java-parser-2
 ln -s ../pom-deploy.xml pom-deploy.xml
 ln -s ../settings-deploy.xml settings-deploy.xml
+ls -al
+
+openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
+openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
+
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
 
-  #openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
-  #openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
+  openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
+  openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
   mvn -f pom-deploy.xml --settings settings-deloy.xml install
 else
   mvn -f pom-deploy.xml --settings settings-deloy.xml install

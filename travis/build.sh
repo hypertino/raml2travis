ecd7a83f90f0427e6a03e4e2abf77d994cc54141
Maga Abdurakhmanov <maga@inn.eu>
2016-02-05 15:24:08 +0300

    * working on travis -> sonatype deploy

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 set -e
+
 cd raml-java-parser-2
 ln -s ../pom-deploy.xml pom-deploy.xml
 ln -s ../settings.xml settings.xml
@@ -8,7 +9,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
 
   #openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
   #openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
-  mvn -f pom-deploy.xml  install
+  mvn -f pom-deploy.xml --settings settings-deloy.xml install
 else
-  mvn -f pom-deploy.xml  install
+  mvn -f pom-deploy.xml --settings settings-deloy.xml install
 fi

3339ee989237528033ab7e6722069508efd89a25
Maga Abdurakhmanov <maga@inn.eu>
2016-02-05 21:10:11 +0300

    * trying to run tests on travis

--- travis/build.sh
+++ travis/build.sh
@@ -13,7 +13,7 @@ ls -al
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
     mvn versions:set -DnewVersion=0.0.$TRAVIS_BUILD_NUMBER
-    mvn -f pom-deploy.xml --settings settings-deploy.xml clean install -DskipTests=true
+    mvn -f pom-deploy.xml --settings settings-deploy.xml clean install
 else
-    mvn -f pom-deploy.xml --settings settings-deploy.xml clean install -DskipTests=true
+    mvn -f pom-deploy.xml --settings settings-deploy.xml clean install
 fi

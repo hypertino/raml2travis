1e7a033ed38f2bd28a6ad770a925079a35fa0e81
Maga Abdurakhmanov <maga@inn.eu>
2016-02-05 16:26:39 +0300

    * working on travis -> sonatype deploy

--- travis/build.sh
+++ travis/build.sh
@@ -11,9 +11,6 @@ openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-private.enc -out ./
 
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-
-  openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
-  openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
   mvn -f pom-deploy.xml --settings settings-deloy.xml install
 else
   mvn -f pom-deploy.xml --settings settings-deloy.xml install

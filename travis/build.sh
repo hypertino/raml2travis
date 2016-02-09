d15255e8ef8e3b2041982af536b58ea576001a59
Maga Abdurakhmanov <maga@inn.eu>
2016-02-09 11:45:02 +0300

    * trying to fix build

--- travis/build.sh
+++ travis/build.sh
@@ -5,7 +5,7 @@ cd raml-java-parser-2
 cp -rf ../pom-parent.xml ./pom.xml
 cp -rf ../pom-parser.xml ./java-raml1-parser/pom.xml
 cp -rf ../pom-js.xml ./javascript-module-holders/pom.xml
-cp -rrf ../settings-deploy.xml settings.xml
+cp -rrf ../settings-deploy.xml ./settings-deploy.xml
 
 openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
 openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
@@ -18,7 +18,7 @@ ls -al ./javascript-module-holders/
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
     mvn versions:set -DnewVersion=0.0.$TRAVIS_BUILD_NUMBER
     mvn versions:commit
-    mvn clean deploy -DskipTests=true
+    mvn clean deploy -DskipTests=true --settings=./settings-deploy.xml
 else
-    mvn clean install
+    mvn clean install --settings=./settings-deploy.xml
 fi

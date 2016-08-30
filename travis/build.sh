e5cd249d0f293d20b8a4dfa876f96b71a07a9841
Maga Abdurakhmanov <maqdev@gmail.com>
2016-08-31 01:10:33 +0200

    * updated to java based parser

--- travis/build.sh
+++ travis/build.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 set -e
 
-cd raml-java-parser-2
+cd raml-java-parser
 cp -rf ../pom-parent.xml ./pom.xml
-cp -rf ../pom-parser.xml ./java-raml1-parser/pom.xml
-cp -rf ../pom-js.xml ./javascript-module-holders/pom.xml
+cp -rf ../pom-parser.xml ./raml-parser-2/pom.xml
+cp -rf ../pom-yagi.xml ./yagi/pom.xml
 cp -rf ../settings-deploy.xml ./settings-deploy.xml
 
 openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
@@ -12,11 +12,11 @@ openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-private.enc -out ./
 gpg --dearmor ./inn-oss-public.asc
 gpg --dearmor ./inn-oss-private.asc
 ls -al
-ls -al ./java-raml1-parser/
-ls -al ./javascript-module-holders/
+ls -al ./raml-parser-2/
+ls -al ./yagi/
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-    mvn versions:set -DnewVersion=0.0.$TRAVIS_BUILD_NUMBER
+    mvn versions:set -DnewVersion=1.0.1.$TRAVIS_BUILD_NUMBER
     mvn clean install -DskipTests=true --settings=./settings-deploy.xml
     mvn deploy -DskipTests=true --settings=./settings-deploy.xml
 else

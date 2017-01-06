c7e8566f4cadcd2bfdf6720177f3a903233a1a95
Maga Abdurakhmanov <maqdev@gmail.com>
2017-01-07 00:33:20 +0100

    - cache (not working), - leave build-number-as-is

--- travis/build.sh
+++ travis/build.sh
@@ -9,7 +9,6 @@ cp -rf ../settings-deploy.xml ./settings-deploy.xml
 
 echo "$key_password" | gpg --passphrase-fd 0 ../travis/ht-oss-public.asc.gpg
 echo "$key_password" | gpg --passphrase-fd 0 ../travis/ht-oss-private.asc.gpg
-cp ../travis/ht-oss-*.asc ./
 gpg --dearmor ./ht-oss-public.asc
 gpg --dearmor ./ht-oss-private.asc
 ls -al
@@ -17,7 +16,7 @@ ls -al ./raml-parser-2/
 ls -al ./yagi/
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-    mvn versions:set -DnewVersion=1.0.5.$TRAVIS_BUILD_NUMBER
+#    mvn versions:set -DnewVersion=1.0.5.$TRAVIS_BUILD_NUMBER
     mvn clean install -DskipTests=true --settings=./settings-deploy.xml
     mvn deploy --settings=./settings-deploy.xml -P sonatype
 else

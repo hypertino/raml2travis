df0d4f9b1bdb58377dfbc0c307cca12f8d1f5b73
Maga Abdurakhmanov <maqdev@gmail.com>
2017-01-07 00:20:40 +0100

    innova -> hypertino

--- travis/build.sh
+++ travis/build.sh
@@ -7,16 +7,17 @@ cp -rf ../pom-parser.xml ./raml-parser-2/pom.xml
 cp -rf ../pom-yagi.xml ./yagi/pom.xml
 cp -rf ../settings-deploy.xml ./settings-deploy.xml
 
-openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
-openssl aes-256-cbc -k "$key_password" -in ../travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
-gpg --dearmor ./inn-oss-public.asc
-gpg --dearmor ./inn-oss-private.asc
+echo "$key_password" | gpg --passphrase-fd 0 ../travis/ht-oss-public.asc.gpg
+echo "$key_password" | gpg --passphrase-fd 0 ../travis/ht-oss-private.asc.gpg
+cp ../travis/ht-oss-*.asc ./
+gpg --dearmor ./ht-oss-public.asc
+gpg --dearmor ./ht-oss-private.asc
 ls -al
 ls -al ./raml-parser-2/
 ls -al ./yagi/
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-    mvn versions:set -DnewVersion=1.0.1.$TRAVIS_BUILD_NUMBER
+    mvn versions:set -DnewVersion=1.0.5.$TRAVIS_BUILD_NUMBER
     mvn clean install -DskipTests=true --settings=./settings-deploy.xml
     mvn deploy --settings=./settings-deploy.xml -P sonatype
 else
